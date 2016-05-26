a485497d5108d11953e3c4523ce0fa8fd023446f
paulBeaudet <inof8or@gmail.com>
2016-05-26 13:22:50 -0400

    suggested dependancies are now include in package.json, some variables have be externalized, should be heroku able, basically still does same thing as before other than that

---
+++ Server/accessBot.js
@@ -0,0 +1,79 @@
+// accessBot.js ~ Copyright 2016 Manchester Makerspace ~ Licence MIT
+/*
+ * General URL structure:
+ * /machineid/card [?supervising=]
+ * machineid is the esp8266 unique serial number associated with the machine in the database
+ *  this is stored so we can perform access control on each piece of equipment
+ *  basically ends up being a query like
+ * SELECT authorized FROM machinetable NATURAL JOIN cards where cardid = thiscardid;
+ * and in the case of supervising, it's the same thing, but looking for full auth.
+ * table machines:
+ * machineID | userID | authorized
+ * table cards:
+ * userID | cardID
+ * ^ Mysql suggestion, Paul knows mongo, mongo is not perfect, it is good enough
+ * we will use mongo unless someone makes a pull request before Paul  :-) 
+ */
+
+var routes = { // singlton for adressing express route request
+    wild: function(req, res){  // when random routes are called
+        console.log('got request at' + req.url);
+        res.status(404).send('this is not the bot you are looking for');
+    },
+    auth: function(req, res){  // when two params are provided in route /:machine/:card
+        // opts = req.query <- from original code: what options should we expect?
+        if(req.params.card === process.env.TESTUSER){
+            console.log("Member trying to access " + req.params.machine);
+            res.send("Member trying to access " + req.params.machine);
+        }
+        else if(req.params.card === process.env.LESSERUSER){
+            console.log("less privilaged trying to access " + req.params.machine);
+            res.send("less privilaged trying to access " + req.params.machine);
+        } else {
+            console.log('No go, bro! Get of my lawn');
+            res.send('No go, bro! Get of my lawn');
+        }
+    },
+    admin: function(req, res){    // when '/admin' is called
+        res.send('this will be an admin page that will be logged into');
+    },
+    register: function(req, res){ // default route
+        res.send('members will register their cards here')
+    }
+}
+
+var cookie = {                                                      // depends on client-sessions
+    session: require('client-sessions'),                            // mozilla's cookie library
+    ingredients: {                                                  // personally I prefer chocolate chips
+        cookieName: 'session',                                      // guess we could call this something different
+        secret: process.env.SESSION_SECRET,                         // do not track secret in version control
+        duration: 365 * 24 * 60 * 60 * 1000,                        // cookie times out in x amount of time
+    },
+    meWant: function(){return cookie.session(cookie.ingredients);}, // nom nom nom!
+    decode: function(content){return cookie.session.util.decode(cookie.ingredients, content);},
+}
+
+
+var serve = {                                                // singlton for server setup
+    express: require('express'),                             // server framework library 
+    parse: require('body-parser'),                           // JSON parsing library
+    theSite: function (){                                    // methode call to serve site
+        var app = serve.express();                           // create famework object
+        var http = require('http').Server(app);              // http server for express framework
+        app.use(require('compression')());                   // gzipping for requested pages
+        app.use(serve.parse.json());                         // support JSON-encoded bodies
+        app.use(serve.parse.urlencoded({extended: true}));   // support URL-encoded bodies
+        app.use(cookie.meWant());                            // support for cookies
+        app.use(require('csurf')());                         // Cross site request forgery tokens
+        app.use(serve.express.static(__dirname + '/views')); // serve page dependancies (sockets, jquery, bootstrap)
+        var router = serve.express.Router();                 // create express router object to add routing events to
+        router.get('/', routes.register);                    // registration route
+        router.get('/admin', routes.admin);                  // web page control for adminstrative functions
+        router.get('/:machine/:card', routes.auth);          // authentication route
+        router.get('/*', routes.wild);                       // catches malformed request and logs them
+        app.use(router);                                     // get express to user the routes we set
+        http.listen(process.env.PORT);                       // listen on specified PORT enviornment variable
+    }
+}
+
+serve.theSite(); // Initiate site!
